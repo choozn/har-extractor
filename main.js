2e5678a801cc9fbf4635266cdbdd77540cc0f05d
choozn <choozn@protonmail.com>
2021-09-13 20:30:47 +0200

    Extracted Replacement of obsolete MIME-Types into seperate config file + Minor Improvements

--- main.js
+++ main.js
@@ -1,120 +1,130 @@
 const { app, clipboard, dialog, shell } = require('electron')
 const { mkdirSync, writeFileSync, existsSync } = require('fs');
-var mimeDB = require('mime-db')
+var mimeDB = require('mime-db');
+var obsoleteMimeTypes = require('./config/obsolete-mime-types.json');
 
 app.whenReady().then(() => {
-  main();
-});
-
-const main = async () => {
 
   try {
 
-    try {
-
-      // Grab HAR data from the clipboard.
-      var clipboardData = clipboard.readText();
-      // Get root of harData
-      var harData = (JSON.parse(String(clipboardData)))["log"];
-      // Remove illegal characters from subfolder name
-      var harTitle = harData["pages"][0]["title"].replace(/(\W+)/gi, '-') || "HAR-Export";
-
-      // Extract all needed information from provided HAR data.
-      var parsedHarData = harData["entries"].map((entry) => {
-
-        // Get filename from url
-        let name = new URL(entry["request"]["url"])
-          .pathname.split('/').pop()
-
-        // Get name without extension
-        if (name.includes(".")) name = name.substring(0, name.lastIndexOf("."));
-
-        return ({
-          data: entry["response"]["content"]["text"] || "",
-          encoding: entry["response"]["content"]["encoding"] || "utf8",
-          type: fixMimeTypes((entry["response"]["content"]["mimeType"] || "text/plain").split(';').shift()),
-          name: name || "index",
-        })
-
-      });
-
-      var nameArray = parsedHarData.map((entry) => entry.name);
-      var duplicateRenamedNameList = suffixDuplicates(nameArray);
-      var duplicateRenamedObject = parsedHarData.map((entry, index) => ({ ...entry, name: duplicateRenamedNameList[index] }));
-
-    } catch (error) {
-
-      // Try-Catch will throw when the provided data did not match the HAR-JSON structure
-      dialog.showErrorBox("", "There was no valid HAR data in your clipboard!");
-      app.quit();
-      return;
-
-    }
-
-    if (parsedHarData) {
-
-      // Select base path for the extraction
-      var baseFolder = (await dialog.showOpenDialog({ properties: ['openDirectory'], buttonLabel: "Extract here" }))["filePaths"][0];
-
-      // If cancel was clicked
-      if (!baseFolder) {
-        app.quit();
-        return;
-      }
-
-      var folder = baseFolder + "/" + harTitle;
-
-      mkdirSync(folder, { recursive: true });
-
-      // Await the extraction of all files
-      await Promise.all(duplicateRenamedObject.map(async (entry) => {
-
-        if (entry.data) {
-
-          try {
-
-            let buffer = Buffer.from(entry.data, entry.encoding);
-            let extension = mimeDB[entry.type].extensions[0];
-
-            return writeFileSync(`${folder}/${entry.name}.${extension}`, buffer);
-
-            // No extension was found for the provided data
-          } catch (error) {
-
-            let buffer = Buffer.from(entry.data, entry.encoding);
-            return writeFileSync(`${folder}/${entry.name}`, buffer);
-
-          }
-
-        }
-
-      }));
-
-      // Open created folder with explorer
-      if (existsSync(folder)) shell.openPath(folder);
-      app.quit();
-      return;
-
-    } else {
-      app.quit();
-      return;
-    }
+    // Main Thread
+    main();
 
   } catch (error) {
 
+    // Unknown error in the main thread
     dialog.showErrorBox("", "There was a problem while extracting the data!\n" + error);
     app.quit();
     return;
 
   }
 
+});
+
+const main = async () => {
+
+  try {
+
+    // Grab HAR data from the clipboard.
+    var clipboardData = clipboard.readText();
+    // Get root of harData
+    var harData = (JSON.parse(String(clipboardData)))["log"];
+    // Remove illegal characters from subfolder name
+    var harTitle = harData["pages"][0]["title"].replace(/(\W+)/gi, '-') || "HAR-Export";
+
+    // Extract all needed information from provided HAR data.
+    var parsedHarData = harData["entries"].map((entry) => {
+
+      // Get filename from url
+      let name = new URL(entry["request"]["url"])
+        .pathname.split('/').pop();
+
+      // Get name without extension
+      if (name.includes(".")) name = name.substring(0, name.lastIndexOf("."));
+
+      return ({
+        data: entry["response"]["content"]["text"] || "",
+        encoding: entry["response"]["content"]["encoding"] || "utf8",
+        type: fixMimeTypes((entry["response"]["content"]["mimeType"] || "text/plain").split(';').shift()),
+        name: name || "index",
+      })
+
+    });
+
+    // Suffixing items with multiple occurrences
+    var nameArray = parsedHarData.map((entry) => entry.name);
+    var duplicateRenamedNameList = suffixDuplicates(nameArray);
+    var duplicateRenamedObject = parsedHarData.map((entry, index) => ({ ...entry, name: duplicateRenamedNameList[index] }));
+
+    // The provided data did not match the HAR-JSON structure
+  } catch (error) {
+
+    dialog.showErrorBox("", "There was no valid HAR data in your clipboard!");
+    app.quit();
+    return;
+
+  }
+
+  if (parsedHarData) {
+
+    // Select base path for the extraction
+    var baseFolder = (await dialog.showOpenDialog({ properties: ['openDirectory'], buttonLabel: "Extract here" }))["filePaths"][0];
+
+    // If cancel was clicked
+    if (!baseFolder) {
+      app.quit();
+      return;
+    }
+
+    var folder = `${baseFolder}/${harTitle}`;
+
+    mkdirSync(folder, { recursive: true });
+
+    // Await the extraction of all files
+    await Promise.all(duplicateRenamedObject.map(async (entry) => {
+
+      if (entry.data) {
+
+        try {
+
+          let buffer = Buffer.from(entry.data, entry.encoding);
+          let extension = mimeDB[entry.type].extensions[0];
+
+          return writeFileSync(`${folder}/${entry.name}.${extension}`, buffer);
+
+          // No extension was found for the provided data
+        } catch (error) {
+
+          let buffer = Buffer.from(entry.data, entry.encoding);
+          return writeFileSync(`${folder}/${entry.name}`, buffer);
+
+        }
+
+      }
+
+    }));
+
+    // Open created folder with explorer
+    if (existsSync(folder)) shell.openPath(folder);
+    app.quit();
+    return;
+
+  } else {
+    app.quit();
+    return;
+  }
+
   // Helper function to fix export bugs caused by obsolete MIME-Types
   function fixMimeTypes(type) {
 
-    let fixedType = type
-      .replace("image/jpg", "image/jpeg")
-      .replace("application/x-javascript", "application/javascript")
-      .replace("text/javascript", "application/javascript")
+    let workingType = type;
+
+    // Replace obsolete MIME-Types with fixed types
+    obsoleteMimeTypes.forEach(obsoleteType => {
+      workingType = workingType.replace(obsoleteType[0], obsoleteType[1]);
+    });
+
+    let fixedType = workingType;
 
     return fixedType;
 
@@ -122,6 +132,7 @@ const main = async () => {
 
 }
 
+// Function to suffix duplicates in an array
 function suffixDuplicates(arrayWithoutSuffixes) {
 
   var list = arrayWithoutSuffixes;
