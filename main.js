cb8c95a3f5f9ec32443032233f4a73f28b238697
choozn <choozn@protonmail.com>
2021-09-15 17:17:25 +0200

    Fixed Rename Bug, Cleaned Up Code, Minor Fixes

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const { app, clipboard, dialog, shell } = require('electron')
 const { mkdirSync, writeFileSync, existsSync } = require('fs');
-var mimeDB = require('mime-db');
-var obsoleteMimeTypes = require('./config/obsolete-mime-types.json');
+const mimeDB = require('mime-db');
+const config = require('./config/config.json');
 
 app.whenReady().then(() => {
 
@@ -21,40 +21,14 @@ app.whenReady().then(() => {
 
 });
 
-const main = async () => {
+/* Main Thread */
+async function main() {
 
   try {
 
     // Grab HAR data from the clipboard.
     var clipboardData = clipboard.readText();
-    // Get root of harData
-    var harData = (JSON.parse(String(clipboardData)))["log"];
-    // Remove illegal characters from subfolder name
-    var harTitle = (harData["pages"][0]["title"] || "HAR-Export").replace(/(\W+)/gi, '-');
-
-    // Extract all needed information from provided HAR data.
-    var parsedHarData = harData["entries"].map((entry) => {
-
-      // Get filename from url
-      let name = new URL(entry["request"]["url"])
-        .pathname.split('/').pop();
-
-      // Get name without extension
-      if (name.includes(".")) name = name.substring(0, name.lastIndexOf("."));
-
-      return ({
-        data: entry["response"]["content"]["text"] || "",
-        encoding: entry["response"]["content"]["encoding"] || "utf8",
-        type: fixMimeTypes((entry["response"]["content"]["mimeType"] || "text/plain").split(';').shift()),
-        name: name || "index",
-      })
-
-    });
-
-    // Suffixing items with multiple occurrences
-    var nameArray = parsedHarData.map((entry) => entry.name);
-    var duplicateRenamedNameList = suffixDuplicates(nameArray);
-    var duplicateRenamedObject = parsedHarData.map((entry, index) => ({ ...entry, name: duplicateRenamedNameList[index] }));
+    var { entries: harEntries, title: harTitle } = await extractHarData(clipboardData);
 
     // The provided data did not match the HAR-JSON structure
   } catch (error) {
@@ -65,63 +39,117 @@ const main = async () => {
 
   }
 
-  if (parsedHarData) {
+  // Select base path for the extraction
+  var baseFolder = (await dialog.showOpenDialog({ properties: ['openDirectory'], buttonLabel: "Extract here" }))["filePaths"][0];
 
-    // Select base path for the extraction
-    var baseFolder = (await dialog.showOpenDialog({ properties: ['openDirectory'], buttonLabel: "Extract here" }))["filePaths"][0];
-
-    // If cancel was clicked
-    if (!baseFolder) {
-      app.quit();
-      return;
-    }
-
-    var folder = `${baseFolder}/${harTitle}`;
-
-    mkdirSync(folder, { recursive: true });
-
-    // Await the extraction of all files
-    await Promise.all(duplicateRenamedObject.map(async (entry) => {
-
-      if (entry.data) {
-
-        let buffer = Buffer.from(entry.data, entry.encoding);
-
-        let mimeType = mimeDB[entry.type];
-
-        var extension;
-        if (mimeType)
-          extension = mimeDB[entry.type].extensions[0];
-
-        if (extension) {
-          // An extension was found for the provided data and appended
-          return writeFileSync(`${folder}/${entry.name}.${extension}`, buffer);
-        } else {
-          // No extension was found for the provided data
-          return writeFileSync(`${folder}/${entry.name}`, buffer);
-        }
-
-      }
-
-    }));
-
-    // Open created folder with explorer
-    if (existsSync(folder)) shell.openPath(folder);
-    app.quit();
-    return;
-
-  } else {
+  // Cancel was clicked
+  if (!baseFolder) {
     app.quit();
     return;
   }
 
-  // Helper function to fix export bugs caused by obsolete MIME-Types
+  var folder = `${baseFolder}/${harTitle}`;
+
+  mkdirSync(folder, { recursive: true });
+
+  // Await the extraction of all files
+  await Promise.all(harEntries.map(async (entry) => {
+
+    if (entry.data) {
+
+      let buffer = Buffer.from(entry.data, entry.encoding);
+      return writeFileSync(`${folder}/${entry.fileName}`, buffer);
+
+    }
+
+  }));
+
+  // Open created folder with explorer
+  if (existsSync(folder)) shell.openPath(folder);
+  app.quit();
+  return;
+
+
+  /*  Functions */
+
+  /* 
+  
+    Main extraction function 
+  
+  */
+
+  async function extractHarData(data) {
+
+    // Get root of HAR data
+    var harData = (JSON.parse(String(data)))["log"];
+    // Get HAR title and remove illegal characters from it
+    var harTitle = (harData["pages"][0]["title"] || config.defaultFolderName).replace(/(\W+)/gi, '-');
+
+    // Reduce entries to base information
+    var parsedHarData = harData["entries"].map((entry) => {
+
+      var content = entry["response"]["content"];
+
+      // Get filename from url without extension
+      var name = (new URL(entry["request"]["url"]).pathname.split('/').pop()) || config.defaultFileName;
+      if (name.includes(".")) name = name.substring(0, name.lastIndexOf("."));
+
+      // Get MIME type
+      var mimeType = fixMimeTypes((content["mimeType"] || config.defaultMimeType).split(';').shift());
+      var mimeLookup = mimeDB[mimeType];
+
+      // Get MIME extension
+      var extension = (mimeLookup) ? mimeLookup.extensions[0] : config.defaultExtension;
+
+      var fileName = `${name}.${extension}`;
+
+      // Get HAR entry's data
+      var data = content["text"];
+
+      // Get HAR entry's encoding
+      var encoding = content["encoding"] || config.defaultEncoding;
+
+      return ({
+        name: name,
+        fileName: fileName,
+        type: mimeType,
+        extension: extension,
+        encoding: encoding,
+        data: data,
+      })
+
+    });
+
+    // Suffixing items with multiple occurrences
+    var nameArray = parsedHarData.map((entry) => entry.fileName);
+    var suffixArray = createSuffixArray(nameArray);
+    var suffixedHarEntries = parsedHarData.map((entry, index) => (
+      {
+        ...entry,
+        fileName: `${entry.name}${suffixArray[index] || ""}${(entry.extension) ? `.${entry.extension}` : `${config.defaultExtension}`}`
+      }
+    ));
+
+    return {
+      title: harTitle,
+      entries: suffixedHarEntries
+    };
+
+  }
+
+
+  /* 
+  
+    Helper function to fix export bugs caused by obsolete MIME types 
+  
+  */
+
   function fixMimeTypes(type) {
 
     let workingType = type;
 
     // Replace obsolete MIME-Types with fixed types
-    obsoleteMimeTypes.forEach(obsoleteType => {
+    config.obsoleteMimeTypes.forEach(obsoleteType => {
       workingType = workingType.replace(obsoleteType[0], obsoleteType[1]);
     });
 
@@ -131,38 +159,45 @@ const main = async () => {
 
   }
 
-}
 
-// Function to suffix duplicates in an array
-function suffixDuplicates(arrayWithoutSuffixes) {
+  /* 
 
-  var list = arrayWithoutSuffixes;
+    Helper function to suffix duplicates in an array - returns an array containing only suffixes
 
-  // Objects to contain occurrences
-  var count = {};
-  var firstOccurrences = {};
+  */
 
-  var item, itemCount;
+  function createSuffixArray(arrayWithoutSuffixes) {
 
-  // Loop through the list
-  for (var i = 0, c = list.length; i < c; i++) {
+    var list = arrayWithoutSuffixes;
+    var suffixArray = [];
 
-    item = list[i];
-    itemCount = count[item];
-    itemCount = count[item] = (itemCount == null ? 1 : itemCount + 1);
+    // Objects to collect occurrences
+    var count = {};
+    var firstOccurrences = {};
 
-    if (itemCount == 2)
-      // First occurrence of an item which has multiple occurrences
-      list[firstOccurrences[item]] = `${list[firstOccurrences[item]]}-(1)`;
-    if (count[item] > 1)
-      // All other occurrences
-      list[i] = `${list[i]}-(${count[item]})`;
-    else
-      // Item without any other occurrences
-      firstOccurrences[item] = i;
+    var item, itemCount;
+
+    // Loop through the list
+    for (var i = 0, c = list.length; i < c; i++) {
+
+      item = list[i];
+      itemCount = count[item];
+      itemCount = count[item] = (itemCount == null ? 1 : itemCount + 1);
+
+      if (itemCount == 2)
+        // First occurrence of an item which has multiple occurrences
+        suffixArray[firstOccurrences[item]] = `-(1)`;
+      if (count[item] > 1)
+        // All other occurrences of an item which has multiple occurrences
+        suffixArray[i] = `-(${count[item]})`;
+      else
+        // Item without any other occurrences
+        firstOccurrences[item] = i;
+
+    }
+
+    return suffixArray;
 
   }
-
-  return list;
 
 }
